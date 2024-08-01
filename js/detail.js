37559d8cfb405196331f602638df30e297aace11
WonbinLee <wonbin145@gmail.com>
2024-08-01 17:53:04 +0900

    1

--- js/detail.js
+++ js/detail.js
@@ -1,14 +1,11 @@
-fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+// 본인의 API 키를 넣어주셔야 합니다.
+const API_KEY = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+fetch(URL)
     .then((response) => response.json())
-    .then(data => {
-        console.log(data)
+    .then((data) => {
+        console.log(data);
+        // 이후 데이터 처리
     })
-  .then((data) => {
-    const movies = data.results;
-    const movieContainer = document.getElementById("movie-container");
-    movies.forEach((movie) => {
-      const card = createMovieCard(movie);
-      movieContainer.appendChild(card);
-    });
-  })
-  .catch((error) => console.error("Error:", error));
+    .catch((error) => console.error("Error:", error));
