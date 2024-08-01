d68c7408791885c41bad61f0474132bf5f265efd
WonbinLee <wonbin145@gmail.com>
2024-08-01 21:59:25 +0900

    1

---
+++ js/detail.js
@@ -0,0 +1,8 @@
+function changeBmkBtne() {
+    const btnElement = document.getElementById("bmkBtn");
+    document.getElementById = "성공!";
+}
+
+$("#bmkBtn").click(changeBmkBtne());
+
+<span class="material-symbols-outlined">bookmark</span>;
