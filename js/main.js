a1c77adc62fbf1ba039a8d21143ae7046068c893
KangSujin <tnwls7138@gmail.com>
2024-08-05 23:01:46 +0900

    메인 페이지 장르별 카드 완료

--- js/main.js
+++ js/main.js
@@ -64,67 +64,70 @@ async function fetchData() {
 
     const mergeMovies = [...movies1, ...movies2, ...movies3, ...movies4, ...movies5, ...movies6, ...movies7, ...movies8, ...movies9, ...movies10];
 
-    mergeMovies.forEach((movie) => {
-        const innerCard = document.querySelectorAll(".movielist-section-happy, .movielist-section-sad, .movielist-section-angry, .movielist-section-anxiety, .movielist-section-cold");
-        for (let i = 0; i < innerCard.length; i++) {
+    console.log("mergeMovies => ", mergeMovies); // 확인완료!
+
+    const genres = [
+        { Id: 28, name: "Action" },
+        { Id: 12, name: "Adventure" },
+        { Id: 16, name: "Animation" },
+        { Id: 35, name: "Comedy" },
+        { Id: 80, name: "Crime" },
+        { Id: 99, name: "Documentary" },
+        { Id: 18, name: "Drama" },
+        { Id: 10751, name: "Family" },
+        { Id: 14, name: "Fantasy" },
+        { Id: 36, name: "History" },
+        { Id: 27, name: "Horror" },
+        { Id: 10402, name: "Music" },
+        { Id: 9648, name: "Mystery" },
+        { Id: 10749, name: "Romance" },
+        { Id: 878, name: "Science Fiction" },
+        { Id: 10770, name: "TV Movie" },
+        { Id: 53, name: "Thriller" },
+        { Id: 10752, name: "War" },
+        { Id: 37, name: "Western" },
+    ];
+
+    // 1) romance, musical 장르의 id를 가져옴
+    // 로직1 : genres에 있는 배열 중, romance, musical이 포함된 id를 추출
+    const genresSearch = function (selectGenres) {
+        const romanceGenres = genres.filter(function (genre) {
+            return genre.name === selectGenres;
+        });
+        return romanceGenres;
+    };
+
+    console.log(genresSearch("Romance"));
+
+    // 2) 1)에서 찾은 장르 id와 일치하는 영화 목록(영화의 genre_ids가 1)에서 뽑은 id를 포함하는지)을 mergeMovies에서 필터링
+    const genreArr = function (genre) {
+        const genreList = mergeMovies.filter(function (movie) {
+            return movie.genre_ids.includes(genre[0].Id);
+        });
+        return genreList;
+    };
+
+    console.log("genreArr => ", genreArr(genresSearch("Romance")));
+
+    const cardMaker = function (genre, classselect) {
+        const innerCard = document.querySelector(`.movielist-section-${classselect}`);
+        genre.forEach((movie) => {
             const cardImg = document.createElement("li");
             cardImg.className = "movie-card";
             cardImg.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"><div class = movie-info>
-        <h3>${movie.title}</h3>
-        ${movie.vote_average}<br></br>${movie.overview}</div>`;
+    <h3>${movie.title}</h3>
+    ${movie.vote_average}<br></br>${movie.overview}</div>`;
+            innerCard.appendChild(cardImg);
+        });
+    };
 
-            innerCard[i].appendChild(cardImg);
-        }
+    cardMaker(genreArr(genresSearch("Romance")), "happy");
+    cardMaker(genreArr(genresSearch("Comedy")), "sad");
+    cardMaker(genreArr(genresSearch("War")), "angry");
+    cardMaker(genreArr(genresSearch("Documentary")), "anxiety");
+    cardMaker(genreArr(genresSearch("Animation")), "cold");
 
-        //장르 가져오기
-        const genres = [{ 28: "Action" }, { 12: "Adventure" }, { 16: "Animation" }, { 35: "Comedy" }, { 80: "Crime" }, { 99: "Documentary" }, { 18: "Drama" }, { 10751: "Family" }, { 14: "Fantasy" }, { 36: "History" }, { 27: "Horror" }, { 10402: "Music" }, { 9648: "Mystery" }, { 10749: "Romance" }, { 878: "Science Fiction" }, { 10770: "TV Movie" }, { 53: "Thriller" }, { 10752: "War" }, { 37: "Western" }];
-
-        const genreId = movie.genre_ids;
-
-        //장르id에 맞는 장르 배열로 가져오기
-        let genreArr = [];
-        for (let i = 0; i < genres.length; i++) {
-            let foundId = genreId.find((key) => key in genres[i]);
-            if (foundId === undefined) {
-                continue;
-            } else {
-                genreArr.push(Object.values(genres[i]));
-            }
-        }
-        //배열 문자열화
-        const toString = function (inputArr) {
-            switch (inputArr.length) {
-                case 1:
-                    return String(inputArr[0]);
-                default:
-                    let genre = inputArr[0];
-                    let i = 1;
-                    while (i < inputArr.length) {
-                        genre += `, ${inputArr[i]}`;
-                        i++;
-                    }
-                    return genre;
-            }
-        };
-        const genre = toString(genreArr);
-
-        //문자열화된 장르 romance, musical 장르 가진 영화들 뽑아내기
-        const happyGenre = genre.includes("Romance", "Musical")
-        const filterhappyGenre = if(happyGenre === true){
-            return 
-        }
-            
-        };
-
-        console.log(happyGenre);
-
-        
-
-
-        //happyGenre 함수 선언 :장르 romance, musical 뽑아내기
-        //true 값을 가지는 genre들 리스트 만들기
-        //리스트 영화 카드에 집어넣기
-    });
+    // cardMaker(happyGenre, "happy");
 }
 
 fetchData().catch((error) => console.error("Error:", error));
