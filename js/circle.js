cf2a24e72c01de4451d6bb29dd57bd25a50dc98d
t2023-m0125 <t2023-m0125@t2023-m0125ui-MacBookAir.local>
2024-08-06 17:22:02 +0900

    fix 파이어베이스 임시주소

--- js/circle.js
+++ js/circle.js
@@ -19,7 +19,7 @@ const db = getFirestore(app);
 console.log(db);
 const urlSearch = new URLSearchParams(location.search);
 const getUrlMovieId = urlSearch.get("movieId");
-const washingtonRef = doc(db, "emotionball", getUrlMovieId);
+const washingtonRef = doc(db, "emotionball", "test");
 
 // Helper function to update circle size
 function updateCircleSize(circle, clickCount) {
